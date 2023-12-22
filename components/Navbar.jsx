c688905e24f44826060b3a844b104f76ca18cdf7
eluminousvijay <eluminous_se88@eluminoustechnologies.com>
2023-12-22 14:13:21 +0530

    changes

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   return (
     <div className="navbar-container">
       <p className="logo">
-        <Link href="/">Vijay Enterprise</Link>
+        <Link href="/">Shaurya KSK</Link>
       </p>
 
       <button type="button" className="cart-icon" onClick={() => setShowCart(true)}>
